0cdf157f890413277d683799cdf2145a0049dd09
GlenLee <isleecn@gmail.com>
2017-11-23 12:55:21 +0800

    清理多余的引用，清理代码
    - 去掉多余的MySQL引用
    - 去掉已无用的Service项目

--- Runtime/NPiculet.Service/Weather/Weather.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace NPiculet.Service.Weather
-{
-	public class Weather
-	{
-		//http://api.map.baidu.com/telematics/v3/weather?location=%E6%98%86%E6%98%8E&output=json&ak=B8cb5720f21c85d3174998ae7a400f7c 
-	}
-}
